4cd47ae88f8319dbfde60f0e6915cb2101036847
Michael L Perry <michael@qedcode.com>
2014-08-29 21:59:41 -0500

    Main page.

--- Festify/Festify/App.cs
+++ Festify/Festify/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Festify.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,14 +14,7 @@ namespace Festify
 		{
             UpdateScheduler.Initialize(a => dispatchAdapter.Invoke(a));
 
-            return new ContentPage
-			{
-				Content = new Label {
-					Text = "Hello, Forms !",
-					VerticalOptions = LayoutOptions.CenterAndExpand,
-					HorizontalOptions = LayoutOptions.CenterAndExpand,
-				},
-			};
+            return new MainPage();
 		}
 	}
 }
---
+++ Festify/Festify/Views/MainPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Festify.Views
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Content = new Label
+            {
+                Text = "Main Page",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+        }
+    }
+}
