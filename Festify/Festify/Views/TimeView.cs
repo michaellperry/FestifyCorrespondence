b9a5adbb49d13c113c3cf71eddf074696ca86ee0
Michael L Perry <michael@qedcode.com>
2014-08-30 20:37:02 -0500

    Layout times in a grid.

--- Festify/Festify/Views/TimeView.cs
+++ Festify/Festify/Views/TimeView.cs
@@ -8,7 +8,7 @@ using Xamarin.Forms;
 
 namespace Festify.Views
 {
-    public class TimeView : StackLayout
+    public class TimeView : Grid
     {
         private readonly TimeHeader _viewModel;
 
@@ -16,10 +16,19 @@ namespace Festify.Views
         {
             _viewModel = viewModel;
 
+            ColumnDefinitions = new ColumnDefinitionCollection
+            {
+                new ColumnDefinition { Width = new GridLength(100, GridUnitType.Absolute) },
+                new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+            };
+
             var label = new Label();
             label.BindText(() => _viewModel.Label);
+            Children.Add(label, 0, 0);
 
-            Children.Add(label);
+            var details = new Label();
+            details.Text = "Breakout Session";
+            Children.Add(details, 1, 0);
         }
 
         public TimeHeader ViewModel
