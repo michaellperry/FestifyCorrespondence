9869681996bd448c77f0c49273aabf08cc90bc00
Michael L Perry <michael@qedcode.com>
2014-09-13 19:17:57 -0500

    Removed schedules, slots, and surveys.

--- Festify.Model/Attendee.cs
+++ Festify.Model/Attendee.cs
@@ -4,9 +4,5 @@ namespace Festify.Model
 {
     public partial class Attendee
     {
-        public Task<Slot> NewSlot(Time time)
-        {
-            return Community.AddFactAsync(new Slot(this, time));
-        }
     }
 }
